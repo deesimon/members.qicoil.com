b2d2601888b5e776a58ae4c313445c7c519b7a34
parth <parth@worldwidewebsolution.com>
2022-11-14 13:40:33 +0530

    changes design

--- sidebar.php
+++ sidebar.php
@@ -2,7 +2,7 @@
     
     <div class="check_listing" >
     <!-- <h5><b>Filter by Category</b></h5> -->
-         <button type="button" class="btn-link collapsed btndrop" data-toggle="collapse" data-target="#demobtn"><h5><b>Filter by Category</b></h5></button>
+         <button type="button" class="btn-link collapsed btndrop" data-toggle="collapse" data-target="#demobtn"><b style="font-size: 15px;">Filter by Category</b></button>
 <div id="demobtn">
   <?
   // print_r($SUBCATEGORIES);
