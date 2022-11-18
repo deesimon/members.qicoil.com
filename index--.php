dd1ae83d61bd900c2270ff07fb132b80cb129b27
Mahesh Yadav <harry2712@gmail.com>
2022-11-18 23:50:47 +0530

    added index.php

---
+++ index--.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>qilifeio</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
+
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/bootstrapslider.min.css">
+
+</head>
+
+<body id="main_home">
+    <div class="container-fluid">
+
+
+        <div class="d-flex justify-content-end"> <img src="images/ic_person_24px.png"></div>
+
+
+
+        <div class="d-flex justify-content-center m4">
+
+            <input type="text" placeholder="Your trialend in 3 days" class="txt_bg col-md-4">
+            <button type="submit" class="btn subcribe "> Subscribe</button>
+
+        </div>
+
+        <div class="d-flex justify-content-center">
+
+
+            <div class="form-group has-search col-lg-7">
+                <span class="f_search"><img src="images/ic_search_24px.png"></span>
+                <input type="text" class="form-control main_search" placeholder="Search">
+            </div>
+        </div>
+        <div class="d-flex">
+
+            <div class="d-flex justify-content-left">
+
+
+                <nav class="nav">
+                    <ul class="col-md-10">
+
+                        <li> <a href="#"> <img src="images/btn_custom_acti.png"></a></li>
+                        <li> <a href="#"> <img src="images/btn_freq_active.png"></a></li>
+                        <li> <a href="#"> <img src="images/btn_programs_ac.png"></a></li>
+                        <li> <a href="#"> <img src="images/btn_shop_active.png"></a></li>
+
+
+                    </ul>
+
+                </nav>
+
+            </div>
+
+
+            <div class="d-flex justify-content-right">
+
+                <div class="user_name">
+                    <h5>Good Morning, Marvin</h5>
+
+                </div>
+                <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                    <!-- Indicators -->
+                    <ol class="carousel-indicators">
+                        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                        <li data-target="#myCarousel" data-slide-to="1"></li>
+                        <li data-target="#myCarousel" data-slide-to="2"></li>
+                    </ol>
+
+                    <!-- Wrapper for slides -->
+                    <div class="carousel-inner">
+                        <div class="item active">
+                            <img src="images/slide1.png" alt="Los Angeles" style="width:100%;">
+                        </div>
+
+                        <div class="item">
+                            <img src="images/slide2.png" alt="Chicago" style="width:100%;">
+                        </div>
+
+                        <div class="item">
+                            <img src="images/slide3.png" alt="New york" style="width:100%;">
+                        </div>
+                    </div>
+
+                    <!-- Left and right controls -->
+                    <a class="left " href="#myCarousel" data-slide="prev">
+
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="right" href="#myCarousel" data-slide="next">
+
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+
+
+            </div>
+        </div>
+
+
+    </div>
+
+</body>
+
+</html>
