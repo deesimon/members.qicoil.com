726dcf3dc47ea449535e9a5b7712aa52849a4661
slopezdee888 <118616440+slopezdee888@users.noreply.github.com>
2022-11-24 08:20:33 +0800

    increase player div

--- inner_frequencies.php
+++ inner_frequencies.php
@@ -235,7 +235,7 @@ foreach ($response->favorite as $v) {
                 </div>
                 <div class="play_box col-md-12">
                   <div class="white_bg1 col-md-12 col-sm-12" id="back_bg">
-                    <div class="col-md-10 pt-5 button_left mt-3 col-xs-9 b_btn">
+                    <div class="col-md-12 pt-5 button_left mt-3 col-xs-12 b_btn">
                       <button type="button" class="stopbtn" id="stopBtn" <?php echo $disabled; ?>><img src=" images/left_btn.png"></button>
                       <button type="button" class="plybtn" onClick="playNote()" id="play" <?php echo $disabled; ?>> <img src="images/middle.png"></button>
                       <button type="button" id="pause"><img src="images/mute.png" <?php echo $disabled; ?>></button>
