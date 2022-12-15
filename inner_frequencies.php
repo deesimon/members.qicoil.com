33b979b6807fd21837b2e8d2bf4f1173c6d1ce22
parth <parth@worldwidewebsolution.com>
2022-12-15 12:57:48 +0530

    test

--- inner_frequencies.php
+++ inner_frequencies.php
@@ -299,7 +299,6 @@ foreach ($response->favorite as $v) {
 
                 <div class="freq-container">
 
-
                   <div class="freq-container__image">
                     <img src="<?php echo (!empty($image) ? 'https://apiadmin.qienergy.ai/assets/uploads/mp3/' . $id . $image : 'images/freaquecy.png'); ?>" width="126" height="126" class="sun">
 
